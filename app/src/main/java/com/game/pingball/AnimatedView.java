503cbe0ca47241dd1bc484e70d41cb8c9967e87f
lioneldg <lioneldegans@gmail.com>
2020-05-06 18:00:51 +0200

    version 06/05/2020

--- app/src/main/java/com/game/pingball/AnimatedView.java
+++ app/src/main/java/com/game/pingball/AnimatedView.java
@@ -9,18 +9,24 @@ import android.util.AttributeSet;
 
 public class AnimatedView extends androidx.appcompat.widget.AppCompatImageView {
     private Context mContext;
-    private int x = -1;
-    private int y = -1;
+    protected int xBall = -1;
+    protected int yBall = -1;
+    protected float xPlatform = -1;
+    protected float yPlatform = -1;
     protected int xVelocity = 5;
     protected int yVelocity = 5;
     private Handler h;
-    private final int FRAME_RATE = 5;
+    private final int FRAME_RATE = 30;
     private BitmapDrawable ball;
     private Bitmap ballBitmap;
+    private BitmapDrawable platform;
+    private Bitmap platformBitmap;
     private int widthScreen;
     private int heightScreen;
-    private int widthBall;
-    private int heightBall;
+    protected int widthBall;
+    protected int heightBall;
+    protected int widthPlatform;
+    protected int heightPlatform;
 
 
 
@@ -32,6 +38,12 @@ public class AnimatedView extends androidx.appcompat.widget.AppCompatImageView {
         ballBitmap = ball.getBitmap();
         widthBall = ball.getBitmap().getWidth();
         heightBall =  ball.getBitmap().getHeight();
+
+        platform = (BitmapDrawable) mContext.getResources().getDrawable(R.drawable.platform);
+        platformBitmap = platform.getBitmap();
+        widthPlatform = platform.getBitmap().getWidth();
+        heightPlatform = platform.getBitmap().getHeight();
+
     }
 
     private Runnable r = new Runnable() {
@@ -41,19 +53,34 @@ public class AnimatedView extends androidx.appcompat.widget.AppCompatImageView {
     };
 
     protected void onDraw(Canvas c) {
-        if (x < 0 && y < 0) {           //on démarre au centre, initialisation de la taille d'écran
-            x = this.getWidth() / 2;
-            y = this.getHeight() / 2;
+        if (xBall < 0 && yBall < 0) {           //on démarre au centre, initialisation de la taille d'écran, initialisation position plateforme
+            xBall = this.getWidth() / 2;
+            yBall = this.getHeight() / 2;
+            xPlatform = this.getWidth()/2-widthPlatform/2;
+            yPlatform = this.getHeight()*0.75f;
             widthScreen = this.getWidth();
             heightScreen = this.getHeight();
+
         }
         else {
-            x += xVelocity;
-            y += yVelocity;
-            if ((x > widthScreen - widthBall) || (x < 0))   xVelocity *= -1;    //on rebondi sur les murs
-            if ((y > heightScreen - heightBall) || (y < 0)) yVelocity *= -1;
+            xBall += xVelocity;
+            yBall += yVelocity;
+            platformTouched();
+            if ((xBall > widthScreen - widthBall) || (xBall < 0))   xVelocity *= -1;    //on rebondi sur les murs
+            if ((yBall > heightScreen - heightBall) || (yBall < 0)) yVelocity *= -1;
         }
-        c.drawBitmap(ballBitmap, x, y, null);
+        c.drawBitmap(ballBitmap, xBall, yBall, null);
+        c.drawBitmap(platformBitmap,xPlatform, yPlatform, null);
         h.postDelayed(r, FRAME_RATE);   //h.postDelayed invalide avec le Runnable ce qui rappelle onDraw
+    }
+
+    private void platformTouched(){
+        if((yBall + heightBall > yPlatform) && ((xBall + widthBall/2.0f > xPlatform) && (xBall + widthBall/2.0f < xPlatform + widthPlatform))){
+            yVelocity*=-1;
+        }
+        if((yBall + heightBall/2.0f > yPlatform) && (yBall + heightBall/2.0f < yPlatform + heightPlatform)){
+            xVelocity *= -1;
+        }
+
     }
 }
--- app/src/main/java/com/game/pingball/MainActivity.java
+++ app/src/main/java/com/game/pingball/MainActivity.java
@@ -4,30 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
+    long oldTimeMillis = 0, newTimeMillis = 0;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final com.game.pingball.AnimatedView animatedView = findViewById(R.id.anim_view);
-      //          animatedView.xVelocity *=-1;
-        //        animatedView.yVelocity *=-1;
 
         animatedView.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(final View v, final MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.d("  main ", " value of x  down" + event.getX());
-                    return true;
-                }
-                else if(event.getAction() == MotionEvent.ACTION_MOVE) {
-                    Log.d("  main ", " value of x  move " + event.getX());
+                if (event.getAction() == MotionEvent.ACTION_DOWN) { return true; }
+                else if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    animatedView.xPlatform = event.getX()-animatedView.widthPlatform/2.0f;
+
+
+
+
+
+                    //&&
+                     //   (event.getX() > animatedView.xBall && event.getX() < (animatedView.xBall + animatedView.widthBall) ) &&
+                       // (event.getY() > animatedView.yBall && event.getY() < (animatedView.yBall + animatedView.heightBall) )){
+                    //empecher de retaper la balle pendant une seconde
+                    //newTimeMillis = Calendar.getInstance().getTimeInMillis();
+                    //if(newTimeMillis - oldTimeMillis > 1000) {
+                      //  animatedView.yVelocity *= -1;
+                        //oldTimeMillis = newTimeMillis;
+                    //}
                     return true;
                 }
                 return false;
