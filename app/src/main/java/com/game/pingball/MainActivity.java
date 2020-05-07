e0c7d14db16d0520bf947ad60e7abe0b163cb9e5
lioneldg <lioneldegans@gmail.com>
2020-05-07 14:42:06 +0200

    version 07/05/2020 activity in Fragment

--- app/src/main/java/com/game/pingball/MainActivity.java
+++ app/src/main/java/com/game/pingball/MainActivity.java
@@ -1,30 +1,23 @@
 package com.game.pingball;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("ClickableViewAccessibility")
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final AnimatedView animatedView = findViewById(R.id.anim_view);
 
-        animatedView.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(final View v, final MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) { return true; }
-                else if(event.getAction() == MotionEvent.ACTION_MOVE){
-                    animatedView.xPlatform = event.getX()-animatedView.widthPlatform/2.0f;
-                    return true;
-                }
-                return false;
-            }
-        });
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        StartFragment startFragment = new StartFragment();
+        ft.add(R.id.main_layout, startFragment);
+        ft.commit();
     }
 }
